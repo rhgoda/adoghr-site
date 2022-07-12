500b71864a15c7470cb51d81d99736b175b5807a
adoghr <adoghr.w@gmail.com>
2022-07-12 14:37:10 +0300

    dqubtc fix

--- pages/dqubtc/main.js
+++ pages/dqubtc/main.js
@@ -9,7 +9,6 @@ let btc = Math.round((spent/oldPrice)*10000000)/10000000;
 let price = fetch("/api/price")
     .then((res) => res.json())
     .then((pric) => {
-        console.log(pric)
         document.getElementById('bought').innerText = btc;
         document.getElementById('spent').innerText = spent;
         document.getElementById('percent').innerText = doRound((1-oldPrice/pric)*100);
